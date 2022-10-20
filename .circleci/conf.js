73e3d895702e24cff9bdbd4e19ea6aff8215f45d
testingprofessional <loeffen19@hotmail.com>
2022-10-20 13:52:05 +0200

    test protractor addition in config.yml

--- .circleci/conf.js
+++ .circleci/conf.js
@@ -1,6 +1,15 @@
 var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 require('dotenv').config();
 exports.config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    restartBrowserBetweenTests: false,
+    framework: 'jasmine',
+    useAllAngular2AppRoots: true,
+    'jasmineNodeOpts': {
+        showColors: true,
+        defaultTimeoutInterval: 100000,
+        isVerbose: true
+    },
     specs: [
         'protractor/testspec.js'
     ]
