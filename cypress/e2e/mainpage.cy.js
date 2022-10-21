ddb05766d2cc0a57a7e3c59f898d2461ab0dfa1c
testingprofessional <loeffen19@hotmail.com>
2022-10-21 09:54:03 +0200

    changed cypress tests

--- cypress/e2e/mainpage.cy.js
+++ cypress/e2e/mainpage.cy.js
@@ -9,18 +9,18 @@ describe('Mainpage check', () => {
     })
   })
 
-  it("verify the status of the site", function() {
+  it("Verify the status of the site", function() {
     cy.request("/").should(function(response) {
       expect(response.status).to.eq(200)
     })
   })
 
-  it("The site cointains 2 links", function() {
+  it("The site contains 2 links", function() {
     cy.contains('qualitygate.nl'),
     cy.contains('qaconsultant.nl')
   })
 
-  it("verfiy the image on the site", function() {
+  it("Verfiy the image on the site", function() {
     console.log(this.sampleData, "Plan")
     cy.get("img")
       .should("have.attr", "src")
